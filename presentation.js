898862b79ac358cc5d7b9cade41038025a52779d
Léo SENIQUE <leo.senique@gmail.com>
2020-07-24 16:15:45 +0200

    ajout options menu supplémentaires

--- presentation.js
+++ presentation.js
@@ -4,24 +4,75 @@ var service = require("./service.js");
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+});
+
+
+async function start() {
+    menu = `
+1. Lister les clients
+2. Ajouter un client
+3. Rechercher un client par nom
+4. Vérifier la disponibilité d'une chambre
+99. Sortir
+`;
+    rl.question(menu, saisie => {
+        switch (saisie) {
+            case "1":
+                console.log("\n>> Liste des clients\n");
+                service.listerClients(clients => {
+                    afficherClients(clients);
+                    start();
+                });
+                break;
+            case "2":
+                var nom,prenoms;
+                rl.question("\nVeuillez renseigner un nom\n", saisie_nom => {
+                    nom = saisie_nom;
+                    rl.question("\nVeuillez renseigner un prenom\n", saisie_prenoms => {
+                        prenoms = saisie_prenoms
+                        var client = {nom: nom, prenoms:prenoms};
+                        service.ajouterClient(client);
+                        start();
+                    }
+                    );
+                });
+                break;
+            case "3":
+                rl.question("\nVeuiller entrer un nom de client\n", saisie_client => {
+                    service.chercherClient(saisie_client, clients => {
+                        afficherClients(clients);
+                        start();
+                    })
+                });
+                break;
+            case "4":
+                rl.question("\nVeuiller entrer le numéro de la chambre\n", saisie_chambre => {
+                    service.chercherChambre(saisie_chambre, chambreDispo => {
+                        messageChambreDispo(saisie_chambre,chambreDispo)
+                        start();
+                    })
+                });   
+                break;            
+            case "99":
+                console.log("\nAu revoir\n");
+                rl.close();
+                break;
+            default:
+                start();
+        }
     });
-
-
-function start(){
-    menu = "\n1. Lister les clients\n99. Sortir\n";
-    rl.question(menu,saisie => {
-        if(saisie == 1){
-            console.log("\n>> Liste des clients\n");
-            service.listerClients();
-            start();
-        }
-        if(saisie == 99){
-            console.log("\nAu revoir\n");
-            rl.close();
-        }
-        else{
-            start();
-        }
-    })
 }
 exports.start = start;
+
+function afficherClients(clients) {
+    clients.forEach(c => console.log(c.nom, c.prenoms));
+}
+
+function messageChambreDispo(numero, disponibilité){
+    if(disponibilité){
+        console.log("La chambre numéro",numero,"est disponible")
+    }
+    else{
+        console.log("La chambre numéro",numero,"n'est pas disponible")
+    }
+}
--- service.js
+++ service.js
@@ -1,11 +1,44 @@
 var request = require('request');
 
-function listerClients() {
+function listerClients(callback) {
     request('https://leo-hotel-web-api.herokuapp.com/clients?start=0&size=10', { json: true }, function (err, res, body) {
         if (err) { return console.log('Erreur', err); }
         // body contient les données récupérées
-        body.forEach(c =>console.log(c.nom, c.prenoms));
+        callback(body);
     });
 }
 
+function ajouterClient(client){
+    request.post('https://leo-hotel-web-api.herokuapp.com/clients', {json:client}, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
+    });
+}
+
+function chercherClient(nom,callback) {
+    request('https://leo-hotel-web-api.herokuapp.com/clients?start=0&size=10', { json: true }, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
+        // body contient les données récupérées
+        callback(body.filter(c => c.nom == nom));
+    });
+
+}
+function chercherChambre(numero,callback) {
+    request('https://leo-hotel-web-api.herokuapp.com/reservations', { json: true }, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
+        // body contient les données récupérées
+        //console.log(body.map(r => r.chambres).map(c => c.numero).some(n => n=="J2"));
+        callback(!Array.prototype.concat.apply([], body.map(r => r.chambres)).map(c => c.numero).some(n => n == numero));
+    });
+
+}
+
+function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+  }
+
 exports.listerClients = listerClients;
+exports.ajouterClient = ajouterClient;
+exports.chercherClient = chercherClient;
+exports.chercherChambre = chercherChambre;
