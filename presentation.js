162de101710f1da0a76d95e690cfcf5d890a91b8
Léo SENIQUE <leo.senique@gmail.com>
2020-07-24 16:16:42 +0200

    petit cleanup

--- presentation.js
+++ presentation.js
@@ -25,12 +25,12 @@ async function start() {
                 });
                 break;
             case "2":
-                var nom,prenoms;
+                var nom, prenoms;
                 rl.question("\nVeuillez renseigner un nom\n", saisie_nom => {
                     nom = saisie_nom;
                     rl.question("\nVeuillez renseigner un prenom\n", saisie_prenoms => {
                         prenoms = saisie_prenoms
-                        var client = {nom: nom, prenoms:prenoms};
+                        var client = { nom: nom, prenoms: prenoms };
                         service.ajouterClient(client);
                         start();
                     }
@@ -48,11 +48,11 @@ async function start() {
             case "4":
                 rl.question("\nVeuiller entrer le numéro de la chambre\n", saisie_chambre => {
                     service.chercherChambre(saisie_chambre, chambreDispo => {
-                        messageChambreDispo(saisie_chambre,chambreDispo)
+                        messageChambreDispo(saisie_chambre, chambreDispo)
                         start();
                     })
-                });   
-                break;            
+                });
+                break;
             case "99":
                 console.log("\nAu revoir\n");
                 rl.close();
@@ -68,11 +68,11 @@ function afficherClients(clients) {
     clients.forEach(c => console.log(c.nom, c.prenoms));
 }
 
-function messageChambreDispo(numero, disponibilité){
-    if(disponibilité){
-        console.log("La chambre numéro",numero,"est disponible")
+function messageChambreDispo(numero, disponibilité) {
+    if (disponibilité) {
+        console.log("La chambre numéro", numero, "est disponible")
     }
-    else{
-        console.log("La chambre numéro",numero,"n'est pas disponible")
+    else {
+        console.log("La chambre numéro", numero, "n'est pas disponible")
     }
 }
--- service.js
+++ service.js
@@ -8,13 +8,13 @@ function listerClients(callback) {
     });
 }
 
-function ajouterClient(client){
-    request.post('https://leo-hotel-web-api.herokuapp.com/clients', {json:client}, function (err, res, body) {
+function ajouterClient(client) {
+    request.post('https://leo-hotel-web-api.herokuapp.com/clients', { json: client }, function (err, res, body) {
         if (err) { return console.log('Erreur', err); }
     });
 }
 
-function chercherClient(nom,callback) {
+function chercherClient(nom, callback) {
     request('https://leo-hotel-web-api.herokuapp.com/clients?start=0&size=10', { json: true }, function (err, res, body) {
         if (err) { return console.log('Erreur', err); }
         // body contient les données récupérées
@@ -22,11 +22,10 @@ function chercherClient(nom,callback) {
     });
 
 }
-function chercherChambre(numero,callback) {
+function chercherChambre(numero, callback) {
     request('https://leo-hotel-web-api.herokuapp.com/reservations', { json: true }, function (err, res, body) {
         if (err) { return console.log('Erreur', err); }
         // body contient les données récupérées
-        //console.log(body.map(r => r.chambres).map(c => c.numero).some(n => n=="J2"));
         callback(!Array.prototype.concat.apply([], body.map(r => r.chambres)).map(c => c.numero).some(n => n == numero));
     });
 
@@ -34,9 +33,9 @@ function chercherChambre(numero,callback) {
 
 function flatten(arr) {
     return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
     }, []);
-  }
+}
 
 exports.listerClients = listerClients;
 exports.ajouterClient = ajouterClient;
